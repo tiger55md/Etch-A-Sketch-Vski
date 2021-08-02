1e4988cc7a75f1bba13b3cb5d8e108b951f2b0e2
tiger55md <karrunucu@gmail.com>
2021-08-02 17:28:16 +0100

    only need to add greyscale

--- sketch.js
+++ sketch.js
@@ -1,25 +1,103 @@
-window.onload = createGrid();
+window.onload= createGrid(32);
 
-function createGrid(){
-    let size = 30;
+function createGrid(gridSize){
     const container = document.querySelector(".container");
-    for(i = 0; i < 16*16; i++){
+    container.style.width = "800px";
+    container.style.height = "800px"
+    let size = container.clientWidth / gridSize;
+
+
+    for(i = 0; i < gridSize*gridSize; i++){
         let divGrid = document.createElement("div");
         divGrid.classList.add("grid");
         divGrid.style.width = `${size}px`;
         divGrid.style.height = `${size}px`;
         divGrid.style.backgroundColor = "white";
-        container.style.maxWidth = `${16*size}px`;
         divGrid.style.display = "inline-block";
         divGrid.style.verticalAlign = "top";
         container.appendChild(divGrid);
     }
+
+
 }
 
-let squares = document.querySelectorAll(".grid");
+function removeCell(){
+    let squares = document.querySelectorAll(".grid");
 
-squares.forEach(function(square){
-    square.onmouseover = function(){
-        this.style.backgroundColor = "black";
+    squares.forEach( square => {
+        square.remove();
+    });
+
+}
+
+
+let resetBtn = document.querySelector(".reset");
+resetBtn.addEventListener("click", function(){
+    let squares = document.querySelectorAll(".grid");
+
+    squares.forEach( square => {
+        square.style.backgroundColor = "white";
+    });
+
+
+});
+
+function sliderUpdater(){
+    let range = document.querySelector(".slider").value;
+    document.querySelector(".display").textContent =`${range}x${range}`;
+}
+
+document.querySelector(".slider").addEventListener('mouseup',function(){
+        sliderUpdater();
+        let range = document.querySelector(".slider").value;
+        removeCell();
+        createGrid(range);
+        makeDrawable();
+
+    });
+
+
+
+
+
+function makeDrawable(){
+    let squares = document.querySelectorAll(".grid");
+
+    squares.forEach(function(square){
+        square.onmouseover = function(){
+            this.style.backgroundColor = ("black");
+        }
+    })
+
+
+}
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
     }
-})
+    return color;
+  }
+
+let blackBtn = document.querySelector(".black");
+blackBtn.addEventListener("click", makeDrawable);
+
+let rainbowBtn = document.querySelector(".rainbow");
+rainbowBtn.addEventListener("click", function(){
+    let squares = document.querySelectorAll(".grid");
+
+    squares.forEach(function(square){
+        square.onmouseover = function(){
+            this.style.backgroundColor = getRandomColor();
+        }
+    });
+});
+
+let chooseBtn = document.querySelector(".greyscale");
+chooseBtn.addEventListener("click",)
+
+
+
+
